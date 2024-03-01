62a3f2af57974ab1ecc2d571e81eec12d43dfc2c
Roy Klip <royklip2@gmail.com>
2024-03-01 15:47:12 +0100

    Update prompt override

--- app/frontend/src/pages/chat/Chat.tsx
+++ app/frontend/src/pages/chat/Chat.tsx
@@ -31,7 +31,7 @@ import { GPT4VSettings } from "../../components/GPT4VSettings";
 
 const Chat = () => {
     const defaultPromptTemplate = [
-        "Assistant helps the company employees with their healthcare plan questions, and questions about the employee handbook. Be brief in your answers.",
+        "Assistant helps the company employees with their questions. Be brief in your answers.",
         "Answer ONLY with the facts listed in the list of sources below. If there isn't enough information below, say you don't know.",
         "Do not generate answers that don't use the sources below. If asking a clarifying question to the user would help, ask the question.",
         "For tabular information return it as an html table. Do not return markdown format. If the question is not in English, answer in the language used in the question.",
