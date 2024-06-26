7cbbf72cbc2580715beaa4954989aaa83a68aabe
Myrthe Lammerse <myrthelammerse@hotmail.com>
2024-06-26 16:34:09 +0200

    revert commit

--- app/frontend/src/pages/chat/Chat.tsx
+++ app/frontend/src/pages/chat/Chat.tsx
@@ -49,6 +49,7 @@ const Chat = (config: typeof cfg) => {
     const configArray = Object.values(config);
     type Usecase = (typeof configArray)[0];
 
+    // const [isConfigPanelOpen, setIsConfigPanelOpen] = useState(false);
     const [isConfigPanelOpen, setIsConfigPanelOpen, currentUsecase] = useOutletContext<[boolean, (arg: boolean) => void, Usecase]>();
     const [promptTemplate, setPromptTemplate] = useState<string>(defaultPromptTemplate);
     const [temperature, setTemperature] = useState<number>(0.3);
